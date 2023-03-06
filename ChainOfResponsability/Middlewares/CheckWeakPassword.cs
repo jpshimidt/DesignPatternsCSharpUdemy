510ca26f9078f1bc67953269a1631de663180a5b
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2023-03-06 00:22:26 -0300

    Init commit

---
+++ ChainOfResponsability/Middlewares/CheckWeakPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChainOfResponsability.Middlewares
+{
+    class CheckWeakPassword : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (password.Equals("1234"))
+            {
+                Console.WriteLine("Senha fraca: Recomendamos a troca da senha");
+                return true;
+            }
+
+            return CheckNext(email, password);
+
+        }
+    }
+}
--- ChainOfResponsability/Program.cs
+++ ChainOfResponsability/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace ChainOfResponsability
+﻿using ChainOfResponsability.Middlewares;
+using ChainOfResponsability.Servers;
+using System;
+
+namespace ChainOfResponsability
 {
     class Program
     {
+        private static Server server;
+
+        static void Init()
+        {
+            server = new Server();
+
+            server.RegisterUser("master@hcode.com.br", "1234");
+            server.RegisterUser("joao@dev.com.br", "1234");
+
+            Middleware middleware = new CheckUserMiddleware(server);
+
+            middleware.LinkWith(new CheckPermissionMiddleware());
+            middleware.LinkWith(new CheckWeakPassword());
+
+            server.SetMiddleware(middleware);
+        }
+
         static void Main(string[] args)
         {
+            Init();
+
+            Boolean done = false;
+
+            do
+            {
+                Console.WriteLine("Digite o seu e-mail:");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Digite a sua senha:");
+                string password = Console.ReadLine();
+
+                done = server.LogIn(email, password);
+            }
+            while (!done);
+
+            Console.ReadLine();
         }
     }
 }
